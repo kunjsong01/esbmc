adf6e4cb03f58b12180f493e162097387d892c36
kunjsong01 <kunjian90@gmail.com>
2021-06-23 19:08:15 +0100

    Fix LValueToRValue cast

--- src/clang-c-frontend/clang_c_convert.cpp
+++ src/clang-c-frontend/clang_c_convert.cpp
@@ -1446,6 +1446,7 @@ bool clang_c_convertert::get_expr(const clang::Stmt &stmt, exprt &new_expr)
     call.function() = callee_expr;
     call.type() = type;
 
+    unsigned num_args = 0;
     for(const clang::Expr *arg : function_call.arguments())
     {
       exprt single_arg;
@@ -1453,7 +1454,9 @@ bool clang_c_convertert::get_expr(const clang::Stmt &stmt, exprt &new_expr)
         return true;
 
       call.arguments().push_back(single_arg);
+      ++num_args;
     }
+    printf("  @@ num_args=%u\n", num_args);
 
     new_expr = call;
     break;
